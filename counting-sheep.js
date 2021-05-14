566118eaf074229537f51d876fb4f57aefb74887
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2021-05-13 23:28:37 -0400

    Create counting-sheep.js

---
+++ counting-sheep.js
@@ -0,0 +1,16 @@
+
+
+
+
+function countSheep(number){
+  //base case
+  if(number === 0){
+    return}
+  
+  console.log(number + "-Another sheep jumps over the fence");
+  
+  countSheep(number -1)
+}
+
+countSheep(3)
+  
