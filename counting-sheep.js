d952eac062514278fccaedca95d01bd309081656
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2021-05-13 23:29:29 -0400

    Update counting-sheep.js

--- counting-sheep.js
+++ counting-sheep.js
@@ -5,7 +5,8 @@
 function countSheep(number){
   //base case
   if(number === 0){
-    return}
+    return 
+  console.log("All sheep have jumped over the fence"}
   
   console.log(number + "-Another sheep jumps over the fence");
   
